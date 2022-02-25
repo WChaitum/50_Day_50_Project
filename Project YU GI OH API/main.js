17473d081d9b42db23ba9fffb10a133e452783a8
WChaitum <vongolafreedom@gmail.com>
2022-02-25 12:44:28 +0700

    Update main.js

--- Project YU GI OH API/main.js
+++ Project YU GI OH API/main.js
@@ -1,39 +1,52 @@
-const API_URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?atk=gt&def=gt'
+const API_URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0&sort=new'
 const IMG_URL = 'https://storage.googleapis.com/ygoprodeck.com/pics/'
+const SEARCH_API = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?&fname='
 const container = document.querySelector('#container')
+// const animated_bgs = document.querySelectorAll('.animated-bg')
 
-// console.log((IMG_URL+'86988864.jpg'));
-getMovies(API_URL)
-async function getMovies(url){
+
+getCards(API_URL)
+async function getCards(url){
     const res = await fetch(url)
     const data = await res.json()
 
     // console.log(data.data[0].name);
     showCard(data.data);
+    
 }
+
 function showCard (Cards){
     // console.log(Cards[0].card_images);
     container.innerHTML = ''
 
-    Cards.forEach((CardData,idx) => {
+    Cards.forEach((CardData) => {
         const{id,name,level,type,race,atk,def,attribute,desc} = CardData
-        // console.log(CardData);
-        console.log(idx);
-        
-        // console.log(`${IMG_URL}${id}.jpg`);
 
+        // console.log(id);
         const ShowImgCard = `${IMG_URL}${id}.jpg`
-        // console.log(ShowImgCard);
-
+        
         const card =  document.createElement('div')
         card.classList.add('card')
-        console.log(card,idx);
         card.innerHTML=`
             <div class="img-container">
                 <img src="${ShowImgCard}" alt="">
             </div>
         `
+        // console.log(ShowImgCard,id);
         container.appendChild(card)
-        card.addEventListener('click',()=>console.log(idx))
+        card.addEventListener('click',()=>console.log(id,name,level,type,race,atk,def,attribute,desc))
+
 })
 }
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const searchTerm = search.value
+    if(searchTerm && searchTerm !== ''){
+        getCards(SEARCH_API + searchTerm)
+
+        search.value = ''
+    }else{
+        window.location.reload()
+    }
+})
